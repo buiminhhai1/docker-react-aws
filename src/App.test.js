45c4dbd6fd1ddbb46e3f8cdcd88cf04c5cf1c7dc
Bùi Minh Hải <39934622+buiminhhai1@users.noreply.github.com>
2020-09-05 23:11:38 +0700

    init code

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders something data link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/nothing here/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
